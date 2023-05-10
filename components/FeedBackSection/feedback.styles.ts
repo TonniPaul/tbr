9420b00c6643ef9c4e2cf1aa97038dc90ace04f1
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-05-10 17:37:04 +0100

    Refactor: Improved  feedback section styling.

--- components/FeedBackSection/feedback.styles.ts
+++ components/FeedBackSection/feedback.styles.ts
@@ -17,41 +17,59 @@ export const TestimonyContainer = styled.section`
     z-index: -1;
 
     @media (min-width: 768px) {
-      height: 90px;
+      height: 80px;
     }
+  }
+  &::before,
+  &::after {
+    border-top-right-radius: 500px;
+    border-top-left-radius: 500px;
   }
 
   &::before {
     left: 0;
     top: -30px;
-    border-top-right-radius: 500px;
-    border-top-left-radius: 500px;
-    transform: rotate(20deg);
+    transform: rotate(30deg);
+
+    @media (min-width: 768px) {
+      transform: rotate(20deg);
+    }
   }
 
   &::after {
     top: -30px;
     right: 0;
-    border-top-left-radius: 500px;
-    border-top-right-radius: 500px;
-    transform: rotate(-20deg);
+    transform: rotate(-30deg);
+
+    @media (min-width: 768px) {
+      transform: rotate(-20deg);
+    }
   }
 
   & p {
     font-style: italic;
 
-    &::before {
+    &::before,
+    &::after {
       content: "";
       position: absolute;
-      width: 50%;
+      width: 60%;
       height: 70px;
       z-index: 1;
       background: var(--light-grey);
       z-index: 1;
       border-radius: 50%;
+      transform: translateX(-50%);
+    }
+
+    &::before {
       top: -40px;
       left: 50%;
-      transform: translateX(-50%);
+    }
+
+    &::after {
+      bottom: -25px;
+      left: 50%;
     }
   }
 
@@ -59,18 +77,20 @@ export const TestimonyContainer = styled.section`
     display: flex;
     align-items: center;
     max-width: 1440px;
-    padding: 1rem 2rem;
+    padding: 1rem 0;
     gap: 20px;
-    /* margin: auto; */
     flex-wrap: nowrap;
     overflow: scroll;
     width: 90%;
     margin: auto;
 
     &::-webkit-scrollbar {
-      height: 5px;
+      height: 2px;
+      width: 0;
     }
-
+    &::-webkit-scrollbar-thumb {
+      background: var(--yellow);
+    }
     & > div {
       min-width: 250px;
       aspect-ratio: 1/1;
