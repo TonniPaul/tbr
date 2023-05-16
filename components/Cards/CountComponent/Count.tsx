4c396cd6098ae0146992e773737e942ed9a31462
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-05-16 18:35:13 +0100

    Fix : Fixed cart page to show increased cart value

--- components/Cards/CountComponent/Count.tsx
+++ components/Cards/CountComponent/Count.tsx
@@ -12,12 +12,14 @@ const Count = ({ quantity, id }: { quantity: number; id: string }) => {
   const { updateQuantity } = useStore();
 
   const handleUnitAdd = () => {
+    setUnit((prev) => prev + 1);
     updateQuantity(id, "increase");
   };
 
   const handleUnitSubtract = () => {
     if (unit === 1) return;
     updateQuantity(id, "decrease");
+    setUnit((prev) => prev - 1);
   };
 
   return (
